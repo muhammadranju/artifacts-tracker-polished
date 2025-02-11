362ede1f28d6c0d0509a98f39b8ea886258ca37f
Muhammad Ranju <mdranju23@gmail.com>
2025-02-11 14:46:53 +0600

    enhance dark mode support in HeroSection component

--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -4,8 +4,8 @@ const HeroSection = () => {
   return (
     <>
       {/* Hero Section */}
-      <section className="lg:bg-gradient-to-b bg-gradient-to-b from-blue-600 via-indigo-600 to-purple-600 text-white">
-        <div className="w-11/12 lg:w-11/12 md:w-11/12 xl:container  mx-auto px-6 py-14 flex  lg:flex-row items-center flex-col-reverse">
+      <section className="lg:bg-gradient-to-b bg-gradient-to-b from-blue-600 via-indigo-600 to-purple-600 text-white dark:from-gray-900 dark:via-gray-800 dark:to-gray-700">
+        <div className="w-11/12 lg:w-11/12 md:w-11/12 xl:container mx-auto px-6 py-14 flex lg:flex-row items-center flex-col-reverse">
           <div className="lg:w-1/2 text-center lg:text-left">
             <h1 className="lg:text-5xl md:text-3xl text-2xl font-extrabold mb-6 leading-tight">
               Explore worldwide artifacts <br /> and their collection history.
@@ -19,7 +19,7 @@ const HeroSection = () => {
             <Link to="/artifacts">
               <button className="relative inline-flex h-12 overflow-hidden rounded-xl p-[3px] focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-offset-2 focus:ring-offset-gray-50">
                 <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
-                <span className="inline-flex h-full w-full cursor-pointer items-center justify-center bg-gray-800 text-sm  backdrop-blur-3xl bg-gradient-to-r from-pink-500 to-yellow-500 px-8 py-3 text-white font-semibold rounded-xl shadow-lg hover:opacity-90">
+                <span className="inline-flex h-full w-full cursor-pointer items-center justify-center bg-gray-800 text-sm backdrop-blur-3xl bg-gradient-to-r from-pink-500 to-yellow-500 px-8 py-3 text-white font-semibold rounded-xl shadow-lg hover:opacity-90">
                   Explore Artifacts
                 </span>
               </button>
